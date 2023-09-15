5ea20ecc4e5c37b7eeca205b5bbbe6d36bde8bc2
Jesse Ilc <jesseilc123@gmail.com>
2023-09-15 15:14:45 -0500

    Added some docker features and started working on front end

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      hello
+    </div>
+  );
+}
+
+export default App;
