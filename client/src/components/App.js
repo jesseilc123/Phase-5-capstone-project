f83694434e27cd4bf25972422306bd72440925a8
Jesse Ilc <jesseilc123@gmail.com>
2023-09-15 23:32:23 -0500

    Created navbar and sidebar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Home from "../pages/Home";
 
 function App() {
   return (
     <div>
-      hello
+      <Navbar />
+      <Sidebar />
+      <Home />
     </div>
   );
 }
